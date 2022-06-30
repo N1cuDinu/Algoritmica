174f16d9aa24fdb1bf61184e1e258b413d361dac
Dinu Neculai <dinuneculai030@gmail.com>
2022-06-30 15:32:12 +0300

    Palindrom1 pt recunoasterea palindromului.

---
+++ Palindrom1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Palindrom1 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = Console.ReadLine();
+            string s2 = ReverseString(s1);
+            bool palindrom = false;
+            if (s1 == s2)
+            {
+                palindrom = true;
+                Console.WriteLine("Este palindrom");
+            }
+            else
+            {
+                Console.WriteLine("Nu este palindrom");
+            }
+            
+
+        }
+
+        public static string ReverseString(string s)
+        {
+            char[] rev = s.ToCharArray();
+            Array.Reverse(rev);
+            return new string(rev);
+        }
+
+    }
+}
