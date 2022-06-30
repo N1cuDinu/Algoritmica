5871e77367d2b719d9e912c67a8cd590fc892c11
Dinu Neculai <dinuneculai030@gmail.com>
2022-06-30 14:06:43 +0300

    ReverseInt Finished

--- ReverseInteger/Program.cs
+++ ReverseInteger/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace ReverseInteger // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti numarul pe care doriti sa il inversati");
+            int x = int.Parse(Console.ReadLine());
+            int raspuns = ReverseInt(x);
+            Console.WriteLine(raspuns);
+        }
+
+        static int ReverseInt(int number)
+        {
+            int reverse=0;
+            while (number != 0)
+            {
+                try {
+                    checked{ 
+                    reverse = reverse * 10 + number % 10;
+                    number = number / 10;
+                    }
+                }
+                catch (System.OverflowException)
+                {
+                    return 0;
+                }
+            }
+            return reverse;
+        }
+    }
+}
