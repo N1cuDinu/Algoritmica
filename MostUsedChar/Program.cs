8dfb1196ffb15278bcd49d48e2f718e7a04e19c7
Dinu Neculai <nicu.dinu.neculai@gmail.com>
2022-07-01 08:32:26 +0300

    MostUsedCharUpdate

--- MostUsedChar/Program.cs
+++ MostUsedChar/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MostUsedChar // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            char caracter = countChar(s);
+            Console.WriteLine(caracter);
+        }
+
+        static char countChar(string s)
+        {
+            int []charCounter = new int[256];
+            for(int i=0; i < s.Length; i++)
+            {
+                charCounter[s[i]]++;
+            }
+            int counter =0;
+            char caract = ' ';
+            for(int i=0; i < s.Length; i++)
+            {
+                if (counter < charCounter[s[i]]) { 
+                    counter = charCounter[s[i]];
+                    caract = s[i];
+                }
+            }
+            return caract;
+        }
+    }
+}
